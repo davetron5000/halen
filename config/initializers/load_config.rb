cab335604d2ec63024cdc733576bfec299e94d4e
Dave Copeland <davetron5000@gmail.com>
2009-08-22 21:35:32 -0400

    avoids barf if Gliffy not installed

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -9,10 +9,15 @@ end
 APP_CONFIG[:wiki_dir] = 'wiki';
 APP_CONFIG[:wiki_root] = APP_CONFIG[:git_root] + '/' + APP_CONFIG[:wiki_dir]
 
-cred = Gliffy::Credentials.new(APP_CONFIG['gliffy_consumer_key'],
-                        APP_CONFIG['gliffy_consumer_secret'],
-                        APP_CONFIG['gliffy_description'],
-                        APP_CONFIG['gliffy_account_id'],
-                        APP_CONFIG['gliffy_username'])
+begin
+  cred = Gliffy::Credentials.new(APP_CONFIG['gliffy_consumer_key'],
+                          APP_CONFIG['gliffy_consumer_secret'],
+                          APP_CONFIG['gliffy_description'],
+                          APP_CONFIG['gliffy_account_id'],
+                          APP_CONFIG['gliffy_username'])
 
-APP_CONFIG[:gliffy] = Gliffy::Handle.new('www.gliffy.com/api/1.0','www.gliffy.com/gliffy',cred)
+  APP_CONFIG[:gliffy] = Gliffy::Handle.new('www.gliffy.com/api/1.0','www.gliffy.com/gliffy',cred)
+rescue Exception => ex
+  Rails.logger.warn("Gliffy plugin is not going to be available: " + ex)
+  APP_CONFIG[:gliffy] = nil
+end
